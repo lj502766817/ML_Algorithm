231a7f23fb0b35689ad544154ebba9f675d088cd
lj502766817 <502766817@qq.com>
2022-06-23 16:37:13 +0800

    Update Something.py

--- KMeans/Something.py
+++ KMeans/Something.py
@@ -1,51 +1,201 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score
 
-from k_means import KMeans
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+np.random.seed(42)
 
-data = pd.read_csv('../data/iris.csv')
-iris_types = ['SETOSA', 'VERSICOLOR', 'VIRGINICA']
+# 做些数据
+# 中心点
+blob_centers = np.array(
+    [[0.2, 2.3],
+     [-1.5, 2.3],
+     [-2.8, 1.8],
+     [-2.8, 2.8],
+     [-2.8, 1.3]])
+# 基于中心点的标准差
+blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
+X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std,
+                  # 随机种子
+                  random_state=7)
 
-x_axis = 'petal_length'
-y_axis = 'petal_width'
 
-# 作一个对比图
-plt.figure(figsize=(12, 5))
-plt.subplot(1, 2, 1)
-for iris_type in iris_types:
-    plt.scatter(data[x_axis][data['class'] == iris_type], data[y_axis][data['class'] == iris_type], label=iris_type)
-plt.title('label known')
-plt.legend()
+def plot_clusters(X, y=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
+    plt.xlabel("$x_1$", fontsize=14)
+    plt.ylabel("$x_2$", fontsize=14, rotation=0)
 
-plt.subplot(1, 2, 2)
-plt.scatter(data[x_axis][:], data[y_axis][:])
-plt.title('label unknown')
+
+plt.figure(figsize=(8, 4))
+plot_clusters(X)
 plt.show()
 
-num_examples = data.shape[0]
-x_train = data[[x_axis, y_axis]].values.reshape(num_examples, 2)
+# 用sklearn的组件进行训练
+k = 5
+kmeans = KMeans(n_clusters=k, random_state=42)
+y_pred = kmeans.fit_predict(X)
+# 模型学习出的聚类结果,fit_predict(X)与kmeans.labels_ 得到预测结果是一致的
+print(y_pred)
+print(kmeans.labels_)
+# 模型学习出的中心点
+print(kmeans.cluster_centers_)
+# 进行预测
+# 预测这几个点是属于哪几个簇
+X_new = np.array([[0, 2], [3, 2], [-3, 3], [-3, 2.5]])
+y_pred = kmeans.predict(X_new)
+print(y_pred)
+# 得出这几个点到各个簇的距离
+distance = kmeans.transform(X_new)
+print(distance)
 
-# 指定好训练所需的参数
-num_clusters = 3
-max_iterations = 50
 
-k_means = KMeans(x_train, num_clusters)
-centroids, closest_centroids_ids = k_means.train(max_iterations)
-# 对比结果
-plt.figure(figsize=(12, 5))
-plt.subplot(1, 2, 1)
-for iris_type in iris_types:
-    plt.scatter(data[x_axis][data['class'] == iris_type], data[y_axis][data['class'] == iris_type], label=iris_type)
-plt.title('label known')
-plt.legend()
+# 画样本点
+def plot_data(X):
+    plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
 
-plt.subplot(1, 2, 2)
-for centroid_id, centroid in enumerate(centroids):
-    current_examples_index = (closest_centroids_ids == centroid_id).flatten()
-    plt.scatter(data[x_axis][current_examples_index], data[y_axis][current_examples_index], label=centroid_id)
 
-for centroid_id, centroid in enumerate(centroids):
-    plt.scatter(centroid[0], centroid[1], c='black', marker='x')
-plt.legend()
-plt.title('label KMeans')
+# 画中心点
+def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
+    if weights is not None:
+        centroids = centroids[weights > weights.max() / 10]
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='o', s=30, linewidths=8,
+                color=circle_color, zorder=10, alpha=0.9)
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=3, linewidths=10,
+                color=cross_color, zorder=11, alpha=1)
+
+
+# 画决策边界
+def plot_decision_boundaries(clusterer, X, resolution=1000, show_centroids=True,
+                             show_xlabels=True, show_ylabels=True):
+    mins = X.min(axis=0) - 0.1
+    maxs = X.max(axis=0) + 0.1
+    xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution),
+                         np.linspace(mins[1], maxs[1], resolution))
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                 cmap="Pastel2")
+    plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                linewidths=1, colors='k')
+    plot_data(X)
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom='off')
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft='off')
+
+
+plt.figure(figsize=(8, 4))
+plot_decision_boundaries(kmeans, X)
 plt.show()
+
+# 算法流程
+kmeans_iter1 = KMeans(n_clusters=5, init='random', n_init=1, max_iter=1, random_state=1)
+kmeans_iter2 = KMeans(n_clusters=5, init='random', n_init=1, max_iter=2, random_state=1)
+kmeans_iter3 = KMeans(n_clusters=5, init='random', n_init=1, max_iter=3, random_state=1)
+
+kmeans_iter1.fit(X)
+kmeans_iter2.fit(X)
+kmeans_iter3.fit(X)
+
+plt.figure(figsize=(12, 8))
+# 初始中心点
+plt.subplot(321)
+plot_data(X)
+plot_centroids(kmeans_iter1.cluster_centers_, circle_color='r', cross_color='k')
+plt.title('Update cluster_centers')
+# 初始决策边界
+plt.subplot(322)
+plot_decision_boundaries(kmeans_iter1, X, show_xlabels=False, show_ylabels=False)
+plt.title('Label')
+# 更新中心点
+plt.subplot(323)
+plot_decision_boundaries(kmeans_iter1, X, show_xlabels=False, show_ylabels=False)
+plot_centroids(kmeans_iter2.cluster_centers_, )
+# 更新决策边界
+plt.subplot(324)
+plot_decision_boundaries(kmeans_iter2, X, show_xlabels=False, show_ylabels=False)
+# 更新中心点
+plt.subplot(325)
+plot_decision_boundaries(kmeans_iter2, X, show_xlabels=False, show_ylabels=False)
+plot_centroids(kmeans_iter3.cluster_centers_, )
+# 更新决策边界
+plt.subplot(326)
+plot_decision_boundaries(kmeans_iter3, X, show_xlabels=False, show_ylabels=False)
+
+plt.show()
+
+
+# 算法的不稳定性,初始点的随机对结果的影响
+def plot_clusterer_comparison(c1, c2, X):
+    c1.fit(X)
+    c2.fit(X)
+
+    plt.figure(figsize=(12, 4))
+    plt.subplot(121)
+    plot_decision_boundaries(c1, X)
+    plt.subplot(122)
+    plot_decision_boundaries(c2, X)
+
+
+c1 = KMeans(n_clusters=5, init='random', n_init=1, random_state=11)
+c2 = KMeans(n_clusters=5, init='random', n_init=1, random_state=19)
+plot_clusterer_comparison(c1, c2, X)
+
+# 每个样本到质心的距离的和的平方,可以用来做评估
+print(kmeans.inertia_)
+
+# 找到最佳簇的可行建议
+# 尝试多个k值,然后试试图像上那个突变的点(随着k值的增加,inertia_的值肯定是越来越小的的,所以找突变的点是值得试试的)
+kmeans_per_k = [KMeans(n_clusters=k).fit(X) for k in range(1, 10)]
+inertias = [model.inertia_ for model in kmeans_per_k]
+plt.figure(figsize=(8, 4))
+plt.plot(range(1, 10), inertias, 'bo-')
+plt.axis([1, 8.5, 0, 1300])
+plt.show()
+# 通过轮廓系数去做
+# 𝑎𝑖 : 计算样本i到同簇其他样本的平均距离ai。ai 越小，说明样本i越应该被聚类到该簇。将ai 称为样本i的簇内不相似度。
+# 𝑏𝑖 : 计算样本i到其他某簇Cj 的所有样本的平均距离bij，称为样本i与簇Cj 的不相似度。定义为样本i的簇间不相似度：bi =min{bi1, bi2, ..., bik}
+# s(i)=(b(i)-a(i))/max{b(i),a(i)}
+# si接近1，则说明样本i聚类合理；si接近-1，则说明样本i更应该分类到另外的簇；若si 近似为0，则说明样本i在两个簇的边界上。
+# 用sklearn的组件来做
+print(silhouette_score(X, kmeans.labels_))
+# 轮廓系数里最小两个簇
+silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
+plt.figure(figsize=(8, 4))
+plt.plot(range(2, 10), silhouette_scores, 'bo-')
+plt.show()
+
+# KMeans的问题,做的好的评估系数可能比做的坏的还低
+X1, y1 = make_blobs(n_samples=1000, centers=((4, -4), (0, 0)), random_state=42)
+X1 = X1.dot(np.array([[0.374, 0.95], [0.732, 0.598]]))
+X2, y2 = make_blobs(n_samples=250, centers=1, random_state=42)
+X2 = X2 + [6, -8]
+X = np.r_[X1, X2]
+y = np.r_[y1, y2]
+plot_data(X)
+kmeans_good = KMeans(n_clusters=3, init=np.array([[-1.5, 2.5], [0.5, 0], [4, 0]]), n_init=1, random_state=42)
+kmeans_bad = KMeans(n_clusters=3, random_state=42)
+kmeans_good.fit(X)
+kmeans_bad.fit(X)
+plt.figure(figsize=(10, 4))
+plt.subplot(121)
+plot_decision_boundaries(kmeans_good, X)
+plt.title('Good - inertia = {}'.format(kmeans_good.inertia_))
+
+plt.subplot(122)
+plot_decision_boundaries(kmeans_bad, X)
+plt.title('Bad - inertia = {}'.format(kmeans_bad.inertia_))
