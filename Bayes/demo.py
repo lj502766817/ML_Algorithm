d8e7cd27dc8cca33e8d6960fb8e9e3922a03440b
lj502766817 <502766817@qq.com>
2022-07-06 10:17:31 +0800

    Update demo.py

--- Bayes/demo.py
+++ Bayes/demo.py
@@ -1,5 +1,7 @@
 """
 贝叶斯实现垃圾邮件过滤
+概率函数为P(h|D)=P(h)*P(D|h)/P(D)
+通过朴素贝叶斯的思想,P(D|h)可以分解成每个单词的P(Di|h)的乘积
 """
 import numpy as np
 import re
@@ -16,7 +18,7 @@ def text_parse(input_string):
 
 def create_vocab_list(doc_list):
     """
-    创建去重后的词汇集合
+    构建语料库
     """
     vocab_set = set([])
     for document in doc_list:
@@ -26,7 +28,7 @@ def create_vocab_list(doc_list):
 
 def set_of_word_2_vec(vocab_list, input_set):
     """
-    将邮件中的单词转换成词库的词向量,如果词库中的单词出现在邮件中就标1,否者标0
+    将邮件中的单词转换成语料库对应的的词向量,如果词库中的单词出现在邮件中就标1,否者标0
     """
     return_vec = [0] * len(vocab_list)
     for word in input_set:
@@ -37,7 +39,7 @@ def set_of_word_2_vec(vocab_list, input_set):
 
 def train_nb(train_mat, train_class):
     """
-    进行词向量的模型训练 todo:没懂
+    进行词向量的模型训练
     """
     # 样本数
     num_train_docs = len(train_mat)
@@ -45,28 +47,37 @@ def train_nb(train_mat, train_class):
     num_words = len(train_mat[0])
     # 标志成垃圾邮件的样本比例
     p1 = sum(train_class) / float(num_train_docs)
-    # 做了一个平滑处理
+    # 做了一个拉普拉斯平滑处理,如果把初始值设置成0的话,
+    # 那么做成朴素贝叶斯的概率相乘的时候,某一个单词概率为0的话就会导致整个的概率都是0
     p0_num = np.ones(num_words)
-    # 拉普拉斯平滑
     p1_num = np.ones(num_words)
+    # 通常情况下都是设置成类别个数,
     p0_denom = 2
-    # 通常情况下都是设置成类别个数
     p1_denom = 2
 
     for i in range(num_train_docs):
         if train_class[i] == 1:
             # 垃圾邮件
+            # 垃圾邮件中每个单词出现的次数
             p1_num += train_mat[i]
+            # 垃圾邮件中的总单词个数
             p1_denom += sum(train_mat[i])
         else:
+            # 同理
             p0_num += train_mat[i]
             p0_denom += sum(train_mat[i])
+    # 由于每个单词的概率值都很小,那么只需要比较大小的话,可以去对数概率来比较
+    # 这里就是P(Di|h)
     p1_vec = np.log(p1_num / p1_denom)
     p0_vec = np.log(p0_num / p0_denom)
     return p0_vec, p1_vec, p1
 
 
 def classify_nb(word_vec, p0_vec, p1_vec, p1_class):
+    """
+    做出预测,这里的概率也是取的对数概率,由于是比较大小,所以分母的P(D)可以不管
+    然后取完对数之后,之前的P(Di|h)累乘就变成了对应出现了的字符的累加
+    """
     p1 = np.log(p1_class) + sum(word_vec * p1_vec)
     p0 = np.log(1.0 - p1_class) + sum(word_vec * p0_vec)
     if p0 > p1:
